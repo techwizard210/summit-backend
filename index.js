8ea60bf1842932d7243ef9008de00c5d8eaee86b
wizard <lenni96210@gmail.com>
2024-08-29 17:23:34 +0800

    public

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.options("*", cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-
+app.use(express.static('public'));
 require("dotenv").config();
 
 const port = process.env.PORT;
@@ -17,7 +17,7 @@ const port = process.env.PORT;
 // Configure multer for file storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Directory to save uploaded files
+    cb(null, "public/uploads/"); // Directory to save uploaded files
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`); // Unique file name
@@ -28,8 +28,8 @@ const upload = multer({ storage: storage });
 
 // Create 'uploads' folder if it doesn't exist
 const fs = require("fs");
-if (!fs.existsSync("uploads")) {
-  fs.mkdirSync("uploads");
+if (!fs.existsSync("public/uploads")) {
+  fs.mkdirSync("public/uploads");
 }
 
 // File upload endpoint
